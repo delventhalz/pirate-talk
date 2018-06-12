6ea6b30ea6cdef4dc3f3da69fab51eb69e14c476
Zac Delventhal <zac@bitwise.io>
2018-06-12 09:27:06 -0500

    Add basic web app layout
    Includes an input form for messages, and a column for display blockchain state.
    
    Signed-off-by: Zac Delventhal <zac@bitwise.io>

--- client/src/main.js
+++ client/src/main.js
@@ -4,4 +4,32 @@ require('bootstrap')
 require('../styles/main.scss')
 const m = require('mithril')
 
-m.render(document.getElementById('app'), m('h1', 'YARRRRRRRRRR'))
+const getSubmitter = state => e => {
+  e.preventDefault()
+  console.log(state.message)
+  document.getElementById('message-input').value = ''
+}
+
+const App = {
+  view(vnode) {
+    return m('.container', [
+      m('h1.text-center.mb-5', 'Talk Like a Pirate'),
+      m('.row',
+        m('.col-md',
+          m('form.form-inline',
+            m('form-group',
+              m('label.sr-only', { for: 'message-input' }, 'Message'),
+              m('input.form-control#message-input', {
+                placeholder: 'Enter message',
+                oninput: m.withAttr('value', v => { vnode.state.message = v })
+              }),
+              m('button.btn.btn-primary.ml-1',
+                { onclick: getSubmitter(vnode.state) },
+                'Submit')))),
+        m('.col-md',
+          'YARRR TEST DATA!!!!'))
+    ])
+  }
+}
+
+m.render(document.getElementById('app'), m(App))
