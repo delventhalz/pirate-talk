da484772af51d43554c0a9f0cc20d4bc302fbaf0
Zac Delventhal <zac@bitwise.io>
2018-08-08 17:23:17 -0500

    Add logging to JS TP start up
    Signed-off-by: Zac Delventhal <zac@bitwise.io>

--- processor-javascript/index.js
+++ processor-javascript/index.js
@@ -7,6 +7,7 @@ const VALIDATOR_URL = process.env.VALIDATOR_URL || 'tcp://localhost:4004'
 
 const tp = new TransactionProcessor(VALIDATOR_URL)
 const handler = new PirateHandler()
-
 tp.addHandler(handler)
+
+console.log('YARRRR READY TO TALK LIKE A PIRATE!!!')
 tp.start()
