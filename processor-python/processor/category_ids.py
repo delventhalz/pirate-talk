1b0e6d4af80a4ca10a19068d93afccee2dffa46e
Ben Thayer <benthayer2365@gmail.com>
2019-02-24 04:50:56 -0600

    Added category id stuff

--- processor-python/processor/category_ids.py
+++ processor-python/processor/category_ids.py
@@ -1,9 +1,7 @@
 import handler
 
-CATEGORY_BOND_BUY_ORDERS = 0
-CATEGORY_BOND_SELL_ORDERS = 1
-CATEGORY_CRYPTO_BUY_ORDERS = 2
-CATEGORY_CRYPTO_SELL_ORDERS = 3
+CATEGORY_BUY_ORDERS = 0
+CATEGORY_SELL_ORDERS = 1
 CATEGORY_CRYPTO_TYPES = 4
 CATEGORY_ISSUANCE = 5
 CATEGORY_OWNERS = 6
@@ -16,14 +14,12 @@ CATEGORY_OWNER_CRYPTO_PUBKEYS = 12
 
 ADDRESS_LEGNTH = 70
 
-def add_namespace(method):
-    def namespace_adder(*args, **kwargs):
-        return handler.BondHandler.namespaces[0] + method(*args, **kwargs)
-    return namespace_adder
+def get_category_prefix(category):
+    return handler.BondHandler.namespaces[0] + hex(category)[2:]
 
 def add_category(method, category):
     def category_adder(*args, **kwargs):
-        return hex(category)[2:] + method(*args, **kwargs)
+        return get_category_prefix(category) + method(*args, **kwargs)
     return category_adder
 
 def zero_pad(method):
@@ -33,22 +29,52 @@ def zero_pad(method):
     return padder
 
 def prefixify(method, category):
-    @add_namespace
     @add_category(category)
     @zero_pad
     def prefixer(*args, **kwargs):
         return method(*args, **kwargs)
     return prefixer
 
+@prefixify(CATEGORY_BUY_ORDERS)
+def get_buy_order_address(buy_asset_type_uuid, sell_asset_type_uuid):
+    return buy_asset_type_uuid + sell_asset_type_uuid
+
+@prefixify(CATEGORY_SELL_ORDERS)
+def get_sell_order_address(sell_asset_type_uuid, buy_asset_type_uuid):
+    return sell_asset_type_uuid + buy_asset_type_uuid
+
 @prefixify(CATEGORY_ISSUANCE)
-def get_issuance_address(uuid):
-    return uuid
+def get_issuance_address(issuance_uuid):
+    return issuance_uuid
 
 @prefixify(CATEGORY_BONDS)
-def get_bond_address(uuid):
-    return uuid
+def get_bond_address(bond_uuid):
+    return bond_uuid
 
 @prefixify(CATEGORY_OWNERS)
-def get_owner_address(pubkey):
-    # TODO pubkey is probably a different format
-    return hex(pubkey)[:32][2:]
+def get_owner_address(owner_pubkey):
+    return owner_pubkey[:32]
+
+@prefixify(CATEGORY_OWNER_BONDS)
+def get_owner_bonds_address(owner_pubkey, issuance_uuid):
+    return owner_pubkey[:32] + issuance_uuid
+
+@prefixify(CATEGORY_OWNER_CRYPTOS)
+def get_owner_cryptos_address(owner_pubkey, crypto_type_uuid):
+    return owner_pubkey[:32] + crypto_uuid
+
+@prefixify(CATEGORY_CRYPTO_TYPE)
+def get_crypto_type_address(crypto_type_uuid):
+    return crypto_type_uuid
+
+@prefixify(CATEGORY_ORDERS)
+def get_order_address(order_uuid):
+    return order_uuid
+
+@prefixify(CATEGORY_CLEARERS)
+def get_clearer_address(clearer_pubkey):
+    return clearer_pubkey[:32]
+
+@prefixify(CATEGORY_OWNER_CRYPTO_PUBKEYS)
+def get_owner_crypto_pubkeys_address(owner_pubkey, crypto_type_uuid):
+    return owner_pubkey[:32] + crypto_type_uuid
