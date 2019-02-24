50e285983989950e05c2532c2364c400c7432859
Ben Thayer <benthayer2365@gmail.com>
2019-02-24 05:20:48 -0600

    issue bond method

--- processor-python/processor/actions.py
+++ processor-python/processor/actions.py
@@ -5,52 +5,67 @@ from pirate_preprocessor import category_ids
 
 
 def is_clearer(context, initiator_pubkey):
+    clearer_addr = category_ids.get_clearer_address(initiator_pubkey)
+    state_info = context.get_state([clearer_addr])
+    if state_info[clearer_addr] is None:
+        return False
+    return True
+
+def is_bank(context, bank_pubkey):
+    bank_address = get_owner_address(bank_pubkey)
+    bank_state_info = context.get_state([bank_address])
+    if bank_info[bank_address] is None:
+        return False
+    else:
+        bank_data = json.loads(bank_info[bank_address].decode('utf-8'))
+        if bank_data['type'] != 'bank':
+            return False
     return True
 
 def issue_bond(context, initiator_pubkey, message_dict):
 
-        #check if bank exists
-    new_state_dict = {}
-    # verify clearer has permissions
+    if not is_bank(context, message_dict['bank_pubkey'])
+        return
     if not is_clearer(context, initiator_pubkey):
         return
+
+    new_state_dict = {}
         
     # create bond type
-    issuance_address = category_ids.get_issuance_address(message_dict['uuid'])
-    issuance_data = message_dict.copy()
+    issuance_address = category_ids.get_issuance_address(message_dict['issuance_uuid'])
     issuance_data = {
-            ''
+        'company_pubkey': message_dict['company_pubkey'],
+        'bank_pubkey': message_dict['bank_pubkey'],
+        'num_issued': message_dict['num_issued'],
+        'denomination': message_dict['denomination'],
+        'interest_rate': message_dict['interest_rate'],
+        'maturity_date': message_dict['maturity_date']
     }
-    del issuance_data['uuid']
+
     issuance_data['serials'] = sorted([
                 uuid4().hex for _ in range(message_dict['num_issued'])
             ])
             
-    new_state_dict[issuance_address] = issuance_data  # TODO make this deterministic
+    new_state_dict[issuance_address] = json.dumps(issuance_data, sort_keys=True)
 
     # create bonds
     for bond_uuid in issuance_data['serials']:
-        
         new_state_dict[get_issuance_address(bond_uuid)] = {
-                'bank_uuid': message_dict['bank_uuid'],
+                'owner_uuid': message_dict['bank_uuid'],
                 'issuance_uuid': bond_uuid
         }
 
     # assign bonds to owner
+    bank_bonds_address = category_ids.get_bank_bonds_address(message_dict['bank_pubkey'], message_dict['issuance_uuid'])
+    new_state_dict[bank_bonds_address] = {
+        'num_owned': len(issuance_data['serials']),
+        'serials': issuance_data['serials']
+    }
 
     context.set_state(new_state_dict)
 
-    
-
-
-
-
-def buy_bond(context, initiator_pubkey, message_dict):
-        # verify clearer has permissions
-        # find onwer's bonds
-        #
-
-
-
-
-def traders_trade()
+buy_bonds_otc
+initiate_trade
+cancel_trade
+accept_trade
+add_crypto
